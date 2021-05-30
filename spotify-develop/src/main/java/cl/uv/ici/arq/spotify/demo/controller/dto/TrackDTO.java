723abfad1c22aed41c417214a6570c6fd95aaf82
Jorge Rodriguez <jorge.rodrigueza@alumnos.uv.cl>
2021-05-30 17:43:41 -0400

    ms

--- spotify-develop/src/main/java/cl/uv/ici/arq/spotify/demo/controller/dto/TrackDTO.java
+++ spotify-develop/src/main/java/cl/uv/ici/arq/spotify/demo/controller/dto/TrackDTO.java
@@ -1,28 +1,14 @@
 package cl.uv.ici.arq.spotify.demo.controller.dto;
 
-import java.util.UUID;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
 import lombok.Data;
-
 
 @Data
 public class TrackDTO {
-	@Id
-    @Column(name = "ID", nullable = false)
-    @GeneratedValue
-    private UUID trackId;
-
-    @Column(name = "playlist", nullable = false)
-    private UUID idPlaylist;
-
-    private String title;
-    private Integer duration;
-	private String id;
-	private String id_playlist;
 	
+	
+	private String id;
+	private String title;
+	private String id_playlist;
+	private Integer duration;
 	
 }
